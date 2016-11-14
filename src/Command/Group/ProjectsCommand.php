8c22989e08d7d7f024b201b7bf8675fe1d66aedd
Julien VITTE <j.vitte@alienor.net>
2016-11-14 11:26:55 +0100

    [FEATURE] search projects in a group

--- src/Command/Group/ProjectsCommand.php
+++ src/Command/Group/ProjectsCommand.php
@@ -2,22 +2,47 @@
 
 namespace Pitchart\GitlabHelper\Command\Group;
 
+use Pitchart\Collection\Collection;
+use Pitchart\GitlabHelper\Service\GitlabClient;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class ProjectsCommand extends Command
+class ProjectsCommand extends Command implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     protected function configure()
     {
         $this->setName('group:projects')
             ->setDescription('List groups from gitlab')
+            ->addArgument('group', InputArgument::REQUIRED, 'Group name')
+            ->addArgument('search', InputArgument::OPTIONAL, 'Search criteria for project names', '')
+            ->addOption('orderby', null, InputOption::VALUE_OPTIONAL, 'Order results by <comment>id</comment>, <comment>name</comment>, <comment>path</comment>, <comment>created_at</comment>, <comment>updated_at</comment> or <comment>last_activity_at</comment>')
+            ->addOption('sort', null, InputOption::VALUE_OPTIONAL, 'Return requests sorted in <comment>asc</comment> or <comment>desc</comment> order. Default is <comment>desc</comment>')
         ;
     }
 
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var GitlabClient $gitlabClient */
+        $gitlabClient = $this->container->get('gitlab_client');
 
+        $response = $gitlabClient->request('GET', sprintf('groups/%s/projects', $input->getArgument('group')), array(
+            'query' => [
+                'search' => $input->getArgument('search'),
+            ],
+        ));
+        $datas = \GuzzleHttp\json_decode($response->getBody()->getContents());
+
+        $projects = Collection::from($datas);
+
+        $projects->each(function($project) use ($output) {
+            $output->writeln(sprintf('<info>%s</info> : %s', $project->name, $project->ssh_url_to_repo, $project->description));
+        });
     }
 }
